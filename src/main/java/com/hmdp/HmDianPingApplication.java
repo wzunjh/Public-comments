a463c19ba513234595bb8881e4653738e69baef3
njh <wzunjh@qq.com>
2022-11-05 21:58:09 +0800

    项目初始化

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -10,6 +10,7 @@ public class HmDianPingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HmDianPingApplication.class, args);
+        System.out.println("大众点评项目启动成功........");
     }
 
 }
