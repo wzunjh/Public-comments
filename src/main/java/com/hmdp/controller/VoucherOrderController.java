83ef1bdb6a4196841c219b21a3a36f0e7e8ea3c5
njh <wzunjh@qq.com>
2022-11-18 01:19:34 +0800

    优惠券限时秒杀功能

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -2,10 +2,14 @@ package com.hmdp.controller;
 
 
 import com.hmdp.dto.Result;
+import com.hmdp.service.IVoucherOrderService;
+import com.hmdp.service.IVoucherService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -18,8 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/voucher-order")
 public class VoucherOrderController {
+
+    @Resource
+    private IVoucherOrderService voucherOrderService;
+
     @PostMapping("seckill/{id}")
     public Result seckillVoucher(@PathVariable("id") Long voucherId) {
-        return Result.fail("功能未完成");
+        return voucherOrderService.seckillVoucher(voucherId);
     }
 }
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +26,51 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())){
+            return Result.fail("秒杀尚未开始!");
+        }
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())){
+            return Result.fail("秒杀已结束!");
+        }
+
+        if (voucher.getStock() < 1){
+            return Result.fail("优惠券已抢完!");
+        }
+
+        boolean success = seckillVoucherService.update().setSql("stock=stock-1").eq("voucher_id",voucherId).update();
+
+        if (!success){
+            return Result.fail("抢购失败!");
+        }
+
+        VoucherOrder voucherOrder = new VoucherOrder();
+
+        long orderId = redisIdWorker.nextId("order");
+
+        voucherOrder.setId(orderId);
+
+        Long userId = UserHolder.getUser().getId();
+
+        voucherOrder.setUserId(userId);
+
+        voucherOrder.setVoucherId(voucherId);
+
+        save(voucherOrder);
+
+
+        return Result.ok(orderId);
+    }
 }
