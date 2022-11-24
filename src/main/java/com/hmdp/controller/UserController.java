e46211153791d30614ed6d0d8823fe5590c1342c
njh <wzunjh@qq.com>
2022-11-24 15:31:56 +0800

    登出功能实现

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -60,7 +60,8 @@ public class UserController {
     @PostMapping("/logout")
     public Result logout(){
         // TODO 实现登出功能
-        return Result.fail("功能未完成");
+        UserHolder.removeUser();
+        return Result.ok();
     }
 
     @GetMapping("/me")
