d15cb91d62b0d22f36fba10096c0b441cee036cc
njh <wzunjh@qq.com>
2022-11-27 16:29:01 +0800

    查看笔记功能实现

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.SystemConstants;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+
+
 
 /**
  * <p>
@@ -17,4 +29,44 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private IBlogService blogService;
+
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+                // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(this::queryBlogUser);
+        return Result.ok(records);
+    }
+
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
+
+    @Override
+    public Result queryBlogById(Long id) {
+
+        //1.查询blog
+        Blog blog = getById(id);
+
+        if (blog == null){
+            return Result.fail("笔记已被删除或不存在!");
+        }
+
+        queryBlogUser(blog);
+        return Result.ok(blog);
+    }
 }
